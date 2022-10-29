d87d35845659e9acd4e2190d5b88958c4b536c1e
Rahul2360 <rahulbansal2360@gmail.com>
2022-10-29 19:28:33 +0530

    Component Hierarchy

--- spacex-angular/src/app/pages/dashboard/dashboard.module.ts
+++ spacex-angular/src/app/pages/dashboard/dashboard.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { TableComponent } from './table/table.component';
+import { FiltersComponent } from './filters/filters.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableComponent,
+    FiltersComponent,
+    PaginationComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
--- spacex-angular/src/app/pages/shared/shared.module.ts
+++ spacex-angular/src/app/pages/shared/shared.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { HeaderComponent } from './header/header.component';
 
+const components = [
+  HeaderComponent
+]
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
+  declarations: [components],
   imports: [
     CommonModule,
     SharedRoutingModule
-  ]
+  ],
+  exports: [components]
 })
 export class SharedModule { }
