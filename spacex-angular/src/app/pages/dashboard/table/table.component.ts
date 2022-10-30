179a4d29d3f657b27dd65eb03c67133f1387a3a0
Rahul <rahulbansal2360@gmail.com>
2022-10-30 18:02:00 +0530

    Add orbit and launch status value

--- spacex-angular/src/app/pages/dashboard/table/table.component.ts
+++ spacex-angular/src/app/pages/dashboard/table/table.component.ts
@@ -26,20 +26,34 @@ export class TableComponent implements OnInit {
   ]
   start_index = 0;
   end_index = 0;
+  launch_status_color = {
+    'success': {
+      foreground: '#03543f',
+      background: '#def7ec'
+    },
+    'upcoming': {
+      foreground: '#92400f',
+      background: '#fef3c7'
+    },
+    'failed': {
+      foreground: '#981b1c',
+      background: '#fde2e1'
+    }
+  }
 
   constructor(
     private spinner: NgxSpinnerService
   ) { }
 
   ngOnChanges(changes: SimpleChanges) {
-    if(changes['loadingIndicator']) {
-      if(changes['loadingIndicator'].currentValue) {
+    if (changes['loadingIndicator']) {
+      if (changes['loadingIndicator'].currentValue) {
         this.spinner.show()
       } else {
         this.spinner.hide();
       }
     }
-    if(changes['table_data']) {
+    if (changes['table_data']) {
       this.end_index = this.limit;
     }
   }
