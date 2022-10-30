f1e0c98164aa0ce3b8e1395ee63740c586b839f2
Rahul <rahulbansal2360@gmail.com>
2022-10-30 21:47:56 +0530

    Handle no data output; API filtering added

--- spacex-angular/src/app/pages/dashboard/dashboard.component.ts
+++ spacex-angular/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CommonService } from 'src/app/services/common.service';
 
 @Component({
@@ -10,19 +11,50 @@ export class DashboardComponent implements OnInit {
 
   launches_list = [];
   loadingIndicator: boolean = false;
-
+  filter_present: boolean = false;
   constructor(
-    private _commonService: CommonService
-  ) { }
+    private _commonService: CommonService,
+    private _activatedRoute: ActivatedRoute
 
-  ngOnInit(): void {
-    this.get_all_launches();
+  ) {
+    this._activatedRoute.queryParams.subscribe(res => {
+      console.log(res);
+      this.filter_present = false;
+      if (res['date_filter'] && res['launch_filter'] == undefined) {
+        this.filter_present = true;
+        const payload = {
+          start: res['start_date'],
+          end: res['end_date']
+        }
+        this.get_all_launches(payload);
+      }
+      if (res['launch_filter'] && res['date_filter'] == undefined) {
+        this.filter_present = true;
+        const payload = {
+          launch_success: res['launch_filter'],
+        }
+        this.get_all_launches(payload);
+      }
+      if (res['launch_filter'] && res['date_filter']) {
+        this.filter_present = true;
+        const payload = {
+          launch_success: false,
+          start: res['start_date'],
+          end: res['end_date']
+        }
+        this.get_all_launches(payload);
+      }
+    })
   }
 
-  get_all_launches() {
+  ngOnInit(): void {
+    if(!this.filter_present) {
+      this.get_all_launches({});
+    }
+  }
+
+  get_all_launches(payload: any) {
     this.loadingIndicator = true;
-    const payload = {
-    };
     this._commonService.get_all_launches(payload).subscribe(res => {
       this.loadingIndicator = false;
       this.launches_list = res;
